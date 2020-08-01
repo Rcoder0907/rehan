b0bb091b63b7e728f14ef20981239ad156f15817
Rehan Raza <rcoder123@gmail.com>
2020-08-01 20:31:27 +0530

    create education page when educations not added

--- src/pages/Education/index.jsx
+++ src/pages/Education/index.jsx
@@ -1,9 +1,28 @@
 import React from "react";
 
+import "./style.scss";
+import Heading from "../../components/atoms/Heading";
+import { useSelector } from "react-redux";
+import Button from "../../components/molecules/Button";
+import Text from "../../components/atoms/Text";
+
 const Education = () => {
+  const name = useSelector((state) => state.userName.name);
+
   return (
     <>
-      Education Page
+      <div className={"education"}>
+        <Heading title={`Welcome to ${name}'s educations page`} size={"big"} />
+        <Button
+          title={"Add new education"}
+          className={"education-button"}
+        />
+        <Text
+          size={"big"}
+          className={"education-empty-text"}
+          text={"You have not added educations yet, please add new education"}
+        />
+      </div>
     </>
   );
 };
