2d8a0ff553ce3eadb5814c042d2f0bb5022dc684
Rehan Raza <rcoder123@gmail.com>
2020-08-01 19:37:49 +0530

    basic and global scss style setup

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import * as serviceWorker from "./serviceWorker";
 import { Provider } from "react-redux";
 import { store } from "./redux/store";
 
+import "./assets/styles/index.scss";
 import { BrowserRouter as Router } from "react-router-dom";
 
 ReactDOM.render(
